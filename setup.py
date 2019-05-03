4069af3b55870d5046c442fab56d1a311b356ccb
Andrea Bruschi <moonx2006@gmail.com>
2019-05-03 16:31:28 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+      name="lazypt",
+      version="0.1",
+      author_email="moonx2006@gmail.com",
+      packages=['lazypt'],
+      package_data={},
+      license='MIT'
+      install_requires=['click', 'pyperclip', 'pyyaml'],
+      entry_points={
+        'console_scripts': ['lazypt = lazypt.cli:commands']
+      }
+)
