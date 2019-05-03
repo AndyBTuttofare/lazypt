3c90d8ad9fce99d300a292fcc5c120d95350d0e0
Andrea Bruschi <moonx2006@gmail.com>
2019-05-03 19:01:13 +0200

    Update pathmanager.py

--- lazypt/pathmanager.py
+++ lazypt/pathmanager.py
@@ -3,9 +3,9 @@ import os
 def path_finder():
 
     script_path = os.path.abspath(__file__)
-    script_path = script_path.split("\\")
+    script_path = script_path.split("/")
     le = len(script_path)
-    script_path = '\\'.join(script_path[0:le-1])
-    script_path = script_path + '\\'
+    script_path = '/'.join(script_path[0:le-1])
+    script_path = script_path + '/'
 
     return script_path
